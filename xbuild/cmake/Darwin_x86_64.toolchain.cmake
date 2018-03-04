9572c74321dd21b8f581adcb67ca0532ae89daea
nmq <nmq@example.com>
2018-03-05 01:19:06 +0800

    travis support add travis_install.sh
    simplify cmake toolchains
    
    correct travis_linux
    
    correct travis_linux
    
    osx trivis test
    
    travis works on os and linux

--- xbuild/cmake/Darwin_x86_64.toolchain.cmake
+++ xbuild/cmake/Darwin_x86_64.toolchain.cmake
@@ -2,11 +2,8 @@ set(CMAKE_SYSTEM_NAME Darwin)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-set(DARWIN_TOOLCHAIN_ROOT /usr/local/Cellar/gcc/7.3.0)
-set(DARWIN_BINUTILS_ROOT /usr/local/Cellar/binutils/2.30)
-
-set(CMAKE_C_COMPILER ${DARWIN_TOOLCHAIN_ROOT}/bin/gcc-7)
-set(CMAKE_CXX_COMPILER ${DARWIN_TOOLCHAIN_ROOT}/bin/g++-7)
+set(CMAKE_C_COMPILER gcc-6)
+set(CMAKE_CXX_COMPILER g++-6)
 
 # project. static linking c++ library
-include(${CMAKE_CURRENT_LIgST_DIR}/base.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/base.cmake)
--- xbuild/cmake/travis_Darwin_x86_64.cmake
+++ xbuild/cmake/travis_Darwin_x86_64.cmake
@@ -1,7 +1,1 @@
-set(CMAKE_SYSTEM_NAME Darwin)
-set(CMAKE_SYSTEM_VERSION 1)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
-
-set(RSOCK_LINKER_FLAGS TRUE)
-# project. static linking c++ library
-include(${CMAKE_CURRENT_LIST_DIR}/base.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/Darwin_x86_64.toolchain.cmake)
