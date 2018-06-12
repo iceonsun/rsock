345a178c1debc0a6f0771d8b78c1185d9caa0ca6
nmq <nmq@example.com>
2018-06-13 02:26:04 +0800

    change some test

--- test/test_client.cpp
+++ test/test_client.cpp
@@ -1,9 +1,7 @@
 #include "../client/csock.h"
 
-#define DEV "en0"
-#define TIP "165.227.117.179"
 //#define DEV "lo0"
-//#define TIP "127.0.0.1"
+#define TIP "127.0.0.1"
 //#define TPORTS "20005-20006"
 
 int main(int argc, char **argv) {
